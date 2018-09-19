c084373a17e0e06f334bdebd1eebb3c345adc824
Ronak Chovatiya <ronakchovatiya@Ronaks-MBP.home>
2018-09-19 01:52:25 -0400

    adding api tests

---
+++ cypress/integration/restAPI_tests.spec.js
@@ -0,0 +1,22 @@
+describe ('Go to the Home Page', () => {
+    it('Home Page should open and validate title', () => {
+        cy.visit('https://test-trading.entrex.tzero.com')
+        cy.title().should('include', 'ENTREX' )
+    })
+    it ('1. Feature Login - A valid user is able to login into the application', () => {
+        cy.get('#email-input').type('rchovatiya88@gmail.com')
+        cy.get('#password-input').type('Password01')
+        cy.get('#login-user-btn').click();
+    })
+
+    it ('6. Api Tests - Validate Rest calls adn endpoints are up and running', () => {
+        it('returns JSON', ()=> {
+            cy.request('https://test-api.entrex.tzero.com/qtip/api/getTraderBalances')
+            .its('body')
+            .its('content-type')
+            .should('include', 'application/json')
+            .expect(200)
+        })
+    })
+
+});
